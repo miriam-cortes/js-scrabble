b2b0357236fc112db53e9e24e8ac78c1c2e26409
Miriam Cortes <mcortes.umiami@gmail.com>
2016-11-17 22:29:57 -0800

    don't know what to do anymore...at my wit's end

--- scrabble.js
+++ scrabble.js
@@ -1,64 +1,119 @@
-var Scrabble = function(playerName) {
-  this.playerName = playerName;
-  this.totalScore = 0;
-  console.log("Welcome, " + this.playerName + "! You have " + this.totalScore + " points." );
+var Scrabble = function() {
+  parseWord = function(word) {
+    this.word = word
+    this.wordAsArray = this.word.toUpperCase().split('');
+    return this.wordAsArray;
+  },
+
+  letterPoints = function(letter) {
+    // this.wordScore = 0;
+    if ( ["A","E","I","O","U","L","N","R","S","T"].indexOf(letter) != -1 ) {
+      return 1;
+    } else if ( ["D","G"].indexOf( letter ) != -1 ) {
+      return 2;
+    } else if ( ["B","C","M","P"].indexOf( letter ) != -1 ) {
+      return 3;
+    } else if ( ["F","H","V","W","Y"].indexOf( letter ) != -1 ) {
+      return 4;
+    } else if ( ["K"].indexOf( letter ) != -1 ) {
+      return 5;
+    } else if ( ["J","X"].indexOf( letter ) != -1 ) {
+      return 8;
+    } else if ( ["Q","Z"].indexOf( letter ) != -1 ) {
+      return 10;
+    } else {
+      return console.log("idk! that's not a letter...wtf");
+    }
+  }
 };
 
 Scrabble.prototype.score = function(word) {
-  console.log(this.playerName)
-  console.log(this.totalScore)
-  this.wordScore = 0;
-  var onePointLetters = ["A","E","I","O","U","L","N","R","S","T"];
-  var twoPointLetters = ["D","G"];
-  var threePointLetters = ["B","C","M","P"];
-  var fourPointLetters = ["F","H","V","W","Y"];
-  var fivePointLetters = ["K"];
-  var eightPointLetters = ["J","X"];
-  var tenPointLetters = ["Q","Z"];
   this.word = word;
-
-  if (this.word == "") {
-    return console.log( this.word + " is not a word...??");
-  };
-
-  wordAsArray = word.toUpperCase().split('');
-
-  // return console.log(wordAsArray);
-
-  //go through word and add up points
-  for (var i=0; i<word.length; i++) {
-    if ( onePointLetters.indexOf(wordAsArray[i]) != -1) {
-      this.wordScore++;
-    } else if ( wordAsArray[i].indexOf(twoPointLetters) != -1) {
-      this.wordScore += 2;
-    } else if ( wordAsArray[i].indexOf(threePointLetters) != -1) {
-      this.wordScore += 3;
-    } else if ( wordAsArray[i].indexOf(fourPointLetters) != -1) {
-      this.wordScore += 4;
-    } else if ( wordAsArray[i].indexOf(fivePointLetters) != -1) {
-      this.wordScore += 5;
-    } else if ( wordAsArray[i].indexOf(eightPointLetters) != -1) {
-      this.wordScore += 8;
-    } else if ( wordAsArray[i].indexOf(onePointLetters) != -1) {
-      this.wordScore += 10;
-    } else {
-      this.wordScore = 0;
-      return console.log("That's not a word.");
-    }
+  this.points = 0
+  if ( Scrabble().ValidWordLength(this.word) ) {
+    parseWord(this.word).forEach(function(letter) {
+      this.points += letterPoints(letter);
+    });
   }
 
   //50 bonus points for using all 7 letters
   if ( this.word.length == 7 ) {
-    this.wordScore += 50;
+    this.points += 50;
   }
-  console.log(this.totalScore);
-  this.totalScore += this.wordScore;
-  console.log(this.totalScore);
-  return console.log( "The word " + this.word + " is worth " + this.wordScore + " points. Your score is now " + this.totalScore );
+  return this.points;
 };
+
+Scrabble.prototype.ValidWordLength = function(word) {
+  if (word == "") {
+    console.log( "That's an empty string, dummy." );
+    return false;
+  } else if (word.length > 7) {
+    console.log( word + " is too long for this game." );
+    return false;
+  } else {
+    return true;
+  };
+};
+
+Scrabble.prototype.highestScoreFrom = function( array ) {
+    var max = 0
+    var maxWord = ""
+    array.forEach ( function (word) {
+      if ( score(word) > max ) {
+        max = score(word)
+        maxWord = word
+      } else if ( score(word) == max ) {
+        if ( word.length < maxWord.length ) {
+          maxWord = word;
+        }
+      }
+    });
+    return maxWord
+};
+
+var Player = function(name) {
+  this.name = name,
+  this.plays = [], //if i'm defining the array here then why does it say it's undefined when I try to push it in??
+  this.playerWon = false,
+  this.totalScore = 0
+};
+
+Player.prototype.play = function(word) {
+    if ( this.playerWon ) {return console.log("You've already won. Why are you still playing??");}
+    this.plays.push(word)
+    return new Scrabble().score(word)
+};
+
+  // totalScore = function() {
+  //   this.plays.forEach(function(word) {
+  //     a = new Scrabble()[score(word)]
+  //     this.totalScore += a
+  //   });
+  //   return this.totalScore
+  // }
+  //
+  // hasWon = function() {
+  //   this.playerWon = true if (this.totalScore > 100)
+  //   return this.playerWon
+  // }
+  //
+  // highestScoringWord = function() {
+  //   return new Scrabble()[score(this.plays)]
+  // }
+  //
+  // highestScoringWordScore = function() {
+  //   return new Scrabble()[score(this.highestScoringWord)]
+  // }
+// };
+
 
 module.exports = Scrabble;
 
-var Me = new Scrabble("Miriam");
-// var blankWord = new Me.score("");
-var firstWord = new Me.score("o");
+var newGame = new Scrabble();
+// newGame[score('')];
+// newGame[score("one")]
+// newGame[score("poOp")]
+// newGame[score("po&")]
+
+var Miriam = new Player("Miriam");
+console.log(Miriam.play("one"));
