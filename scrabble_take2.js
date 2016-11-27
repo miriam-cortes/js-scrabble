192567ff707bd0b90437315ad80f738c099ab3b9
Miriam Cortes <mcortes.umiami@gmail.com>
2016-11-26 19:26:57 -0800

    wave 1 completed

--- scrabble_take2.js
+++ scrabble_take2.js
@@ -1,9 +1,5 @@
 var Scrabble = function() {}
 
-// Scrabble.prototype.letterPoints = function(letter) {
-//
-// }
-
 Scrabble.prototype.score = function (word) {
   if ( word.length > 7 ) {
     return console.log("That word is too long for a real scrabble round. You've been Scrabbled!");
@@ -37,9 +33,37 @@ Scrabble.prototype.score = function (word) {
     }
   });
 
-  return console.log(wordPoints);
+  return wordPoints
 };
 
+Scrabble.prototype.highestScoreFrom = function (arrayOfWords) {
+  max = 0
+  maxWord = ""
+  arrayOfWords.forEach( function(word) {
+    currentWordScore = Scrabble.prototype.score(word)
+    if ( currentWordScore > max ) {
+      max = currentWordScore
+      max_word = word
+    } else if ( currentWordScore == max ) {
+      if ( word.length < max_word.length ) {
+        max_word = word
+      }
+    }
+  })
+  return max_word
+};
+
+
+
+///////// TESTING STUFF OUT /////////
 var newGame = new Scrabble()
-// console.log(newGame.parseWord('poop'));
-console.log(newGame.score("poop"));
+// returns the total score value for the given word. The word is input as a string (case insensitive).
+console.log(newGame.score("poop")); //8
+console.log(newGame.score("fuzz")); //25
+console.log(newGame.score("guzzle")); //25
+//if the top score is tied between multiple words, pick the one with the fewest letters.
+console.log(newGame.highestScoreFrom(["poop", "fuzz","guzzle"])); //fuzz
+//If the top score is tied between multiple words and one used all seven letters, choose the one with seven letters over the one with fewer tiles.
+console.log(newGame.highestScoreFrom(["poop", "fuzz","jimjams"])); // jimjams
+//If the there are multiple words that are the same score and same length, pick the first one in supplied list.
+console.log(newGame.highestScoreFrom(["jibb", "jauk","jamb"])); // jibb
